083047259bf50320021c7622479e7dd526afc75a
Burak Canik <canik.burak@gmail.com>
2018-01-20 17:55:48 +0300

    Committed the initial version of the app.

---
+++ SubtitleBulkRenamer/MainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace SubtitleBulkRenamer 
+{ 
+    partial class MainForm 
+    { 
+        /// <summary> 
+        /// Required designer variable. 
+        /// </summary> 
+        private System.ComponentModel.IContainer components = null; 
+ 
+        /// <summary> 
+        /// Clean up any resources being used. 
+        /// </summary> 
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param> 
+        protected override void Dispose( bool disposing ) 
+        { 
+            if( disposing && ( components != null ) ) 
+            { 
+                components.Dispose(); 
+            } 
+            base.Dispose( disposing ); 
+        } 
+ 
+        #region Windows Form Designer generated code 
+ 
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor. 
+        /// </summary> 
+        private void InitializeComponent() 
+        { 
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+			this.MainTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+			this.DragFileLabel = new System.Windows.Forms.Label();
+			this.BrowseButton = new System.Windows.Forms.Button();
+			this.MainTableLayoutPanel.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// OpenFileDialog
+			// 
+			this.OpenFileDialog.Filter = "SRT & SUB files| *.srt; *.sub";
+			this.OpenFileDialog.Multiselect = true;
+			this.OpenFileDialog.SupportMultiDottedExtensions = true;
+			// 
+			// MainTableLayoutPanel
+			// 
+			this.MainTableLayoutPanel.ColumnCount = 1;
+			this.MainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.MainTableLayoutPanel.Controls.Add(this.DragFileLabel, 0, 0);
+			this.MainTableLayoutPanel.Controls.Add(this.BrowseButton, 0, 1);
+			this.MainTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.MainTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+			this.MainTableLayoutPanel.Name = "MainTableLayoutPanel";
+			this.MainTableLayoutPanel.RowCount = 2;
+			this.MainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.8419F));
+			this.MainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.1581F));
+			this.MainTableLayoutPanel.Size = new System.Drawing.Size(428, 339);
+			this.MainTableLayoutPanel.TabIndex = 9;
+			// 
+			// DragFileLabel
+			// 
+			this.DragFileLabel.AllowDrop = true;
+			this.DragFileLabel.AutoSize = true;
+			this.DragFileLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.DragFileLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.DragFileLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+			this.DragFileLabel.ForeColor = System.Drawing.Color.Yellow;
+			this.DragFileLabel.Location = new System.Drawing.Point(3, 0);
+			this.DragFileLabel.Name = "DragFileLabel";
+			this.DragFileLabel.Size = new System.Drawing.Size(422, 270);
+			this.DragFileLabel.TabIndex = 7;
+			this.DragFileLabel.Text = "You can drag files here.";
+			this.DragFileLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.DragFileLabel.DragDrop += new System.Windows.Forms.DragEventHandler(this.OnDragDrop);
+			this.DragFileLabel.DragEnter += new System.Windows.Forms.DragEventHandler(this.OnDragEnter);
+			// 
+			// BrowseButton
+			// 
+			this.BrowseButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.BrowseButton.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.BrowseButton.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+			this.BrowseButton.FlatAppearance.CheckedBackColor = System.Drawing.Color.Black;
+			this.BrowseButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+			this.BrowseButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+			this.BrowseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.BrowseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+			this.BrowseButton.ForeColor = System.Drawing.Color.Yellow;
+			this.BrowseButton.Location = new System.Drawing.Point(3, 273);
+			this.BrowseButton.Name = "BrowseButton";
+			this.BrowseButton.Size = new System.Drawing.Size(422, 63);
+			this.BrowseButton.TabIndex = 6;
+			this.BrowseButton.Text = "Or Browse...";
+			this.BrowseButton.UseVisualStyleBackColor = false;
+			this.BrowseButton.Click += new System.EventHandler(this.OnBrowseButtonClicked);
+			// 
+			// MainForm
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(428, 339);
+			this.Controls.Add(this.MainTableLayoutPanel);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "BulkSubtitleRenamer";
+			this.MainTableLayoutPanel.ResumeLayout(false);
+			this.MainTableLayoutPanel.PerformLayout();
+			this.ResumeLayout(false);
+
+        } 
+ 
+        #endregion 
+ 
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog; 
+        private System.Windows.Forms.TableLayoutPanel MainTableLayoutPanel; 
+        private System.Windows.Forms.Label DragFileLabel; 
+        private System.Windows.Forms.Button BrowseButton; 
+    } 
+} 
---
+++ SubtitleBulkRenamer/MainForm.cs
@@ -0,0 +1,144 @@
+﻿using System; 
+using System.Collections.Generic; 
+using System.ComponentModel; 
+using System.Data; 
+using System.Drawing; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
+using System.Windows.Forms; 
+using System.IO; 
+ 
+namespace SubtitleBulkRenamer 
+{ 
+    public partial class MainForm : Form 
+    { 
+        public MainForm() 
+        { 
+            InitializeComponent(); 
+        } 
+ 
+        private void RenameSubtitles( List< string > subtitleFilenames ) 
+        { 
+            // Get directory path and subtitle extension type. 
+            FileInfo fileInfo  = new FileInfo( subtitleFilenames.First() ); 
+            string   filePath  = fileInfo.FullName; 
+            string   extension = fileInfo.Extension; 
+            string   directory = filePath.Substring( 0, filePath.LastIndexOf( "\\" ) ); 
+ 
+            // Set the directory and get all video files from it so we can iterate through them. 
+            Directory.SetCurrentDirectory( directory ); 
+            List< string > videoFileNames = new List< string >( Directory.GetFiles( directory, "*.mkv" ) ); 
+            if( videoFileNames.Count <= 0 ) // If the video file format is not mkv then it must be mp4. 
+                videoFileNames = new List< string >( Directory.GetFiles( directory, "*.mp4" ) ); 
+            if( videoFileNames.Count <= 0 ) // If the video file format is not mkv OR mp4 then signal error and quit. 
+            { 
+                MessageBox.Show( "No .mkv or .mp4 files found in directory!", "Error Encountered" ); 
+                Application.Exit(); 
+            } 
+ 
+            // Search through video files and sort them based on episode number. 
+            Dictionary< int, string > videoFiles = new Dictionary< int, string >(); 
+            foreach( string fileName in videoFileNames ) 
+            { 
+                string sxxexx =  
+                    System.Text.RegularExpressions.Regex.Match( fileName.ToLower(), "s\\d\\de\\d\\d" ).Value; 
+                if( sxxexx.Length == 0 ) 
+                { 
+                    MessageBox.Show( "Video file didn't have \"SXXEXX\" format!", "Error Encountered" ); 
+                    Application.Exit(); 
+                } 
+                int episodeNo = ( int )Char.GetNumericValue( sxxexx.ElementAt( 4 ) ) * 10 + 
+                                ( int )Char.GetNumericValue( sxxexx.ElementAt( 5 ) ); 
+                videoFiles[ episodeNo ] = fileName; 
+            } 
+ 
+            // Search through subtitles and sort them based on episode number. 
+            Dictionary< int, string > subtitleFiles = new Dictionary< int, string >(); 
+            foreach( string fileName in subtitleFilenames ) 
+            { 
+                string sxxexx =  
+                    System.Text.RegularExpressions.Regex.Match( fileName.ToLower(), "s\\d\\de\\d\\d" ).Value; 
+                if( sxxexx.Length == 0 ) 
+                { 
+                    MessageBox.Show( "Subtitle file didn't have \"SXXEXX\" format!", "Error Encountered" ); 
+                    Application.Exit(); 
+                } 
+                int episodeNo = ( int )Char.GetNumericValue( sxxexx.ElementAt( 4 ) ) * 10 + 
+                                ( int )Char.GetNumericValue( sxxexx.ElementAt( 5 ) ); 
+                subtitleFiles[ episodeNo ] = fileName; 
+            } 
+ 
+            int renamedCount = 0, notRenamedCount = 0; 
+            // Rename the subtitles based on video file names. 
+            foreach( var subtitleFile in subtitleFiles ) 
+            { 
+                int       episodeNo           = subtitleFile.Key; 
+                string oldSubtitleFileName = subtitleFile.Value; 
+                string videoFilename       = videoFiles[ episodeNo ]; 
+                string newSubtitleFileName = videoFilename.Substring( 0, videoFilename.LastIndexOf( '.' ) ) +  
+                                             extension; 
+                if( oldSubtitleFileName.Equals( newSubtitleFileName ) ) 
+                    notRenamedCount++; // Count the number of subtitles with already correct names. 
+                else // Rename. 
+                { 
+                    System.IO.File.Move( oldSubtitleFileName, newSubtitleFileName ); 
+                    renamedCount++; 
+                } 
+            } 
+ 
+            // If execution reached here, no errors so far. Print success message and quit. 
+            string successPrompt = ""; 
+ 
+            if( renamedCount > 0 ) 
+                successPrompt += renamedCount.ToString() + 
+                                 ( renamedCount > 1 ? " files were renamed!" : " file was renamed!" ); 
+            else 
+                successPrompt += "No files were renamed!"; 
+            if( notRenamedCount > 0 ) 
+                successPrompt += "\n" + notRenamedCount.ToString() + 
+                                 ( notRenamedCount > 1 ? " files already had correct names!"  
+                                                       : " file already had the correct name!" ); 
+            MessageBox.Show( successPrompt, "Renaming Finished Successfully" ); 
+        } 
+ 
+        private void OnBrowseButtonClicked( object sender, EventArgs e ) 
+        { 
+            if( OpenFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK ) 
+            { 
+                RenameSubtitles( new List< string >( OpenFileDialog.FileNames ) ); 
+            } 
+        } 
+ 
+        private void OnDragDrop( object sender, DragEventArgs e ) 
+        { 
+            int x = this.PointToClient( new Point( e.X, e.Y ) ).X; 
+ 
+            int y = this.PointToClient( new Point( e.X, e.Y ) ).Y; 
+ 
+            if( x >= DragFileLabel.Location.X && ( x <= DragFileLabel.Location.X + DragFileLabel.Width ) && 
+                y >= DragFileLabel.Location.Y && ( y <= DragFileLabel.Location.Y + DragFileLabel.Height ) ) 
+            { 
+                List< string > fileNames = new List< string >( ( string[] )e.Data.GetData( DataFormats.FileDrop ) ); 
+                // Only subtitles are allowed. Delete other files from the List. 
+                for( int i = 0 ; i < fileNames.Count ; i++ ) 
+                { 
+                    string filenameLowercase = fileNames.ElementAt( i ).ToLower(); 
+                    if( filenameLowercase.IndexOf( ".srt" ) == -1 && filenameLowercase.IndexOf( ".sub" ) == -1 ) 
+                        fileNames.RemoveAt( i ); 
+                } 
+ 
+                if( fileNames.Count <= 0 ) 
+                    MessageBox.Show( "No subtitles were selected! Please choose only subtitle (.SRT & .SUB) files.",  
+                                     "Error Encountered" ); 
+                else 
+                    RenameSubtitles( fileNames ); 
+            } 
+        } 
+ 
+        private void OnDragEnter( object sender, DragEventArgs e ) 
+        { 
+            e.Effect = DragDropEffects.Move; 
+        } 
+    } 
+} 
